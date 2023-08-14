538509f2ef0942a16044720b0e3c7ca1b5d071ef
kanual <141069412+kanual@users.noreply.github.com>
2023-08-13 21:23:31 -0700

    Add files via upload

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Door : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class Door : MonoBehaviour, Interactable{
+    public string UnlockItem;
+
+    private GameObject inventory_var;
+
     void Start()
     {
-        
+        inventory_var = GameObject.Find("inventory");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Interact(DisplayImage currentDisplay){
+        if (inventory_var.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem){
+                Debug.Log("unlock");
+        }
     }
 }
